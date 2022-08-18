5caf78836d0f13f2278c470934b6ffa325078a9d
Tom L <devzemux1613@gmail.com>
2022-08-18 21:09:48 +0200

    feat: add no equation

---
+++ src/main/java/de/tom/equationSolver/EquationSolver.java
@@ -0,0 +1,14 @@
+package de.tom.equationSolver;
+
+public class EquationSolver {
+
+    public static void main(String[] args) {
+
+        if(args.length == 0){
+            System.out.println("Es wurde keine Gleichung angegeben!");
+            System.exit(0);
+        }
+
+    }
+
+}
