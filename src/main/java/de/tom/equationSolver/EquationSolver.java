6afb449b9e4250ed3d41ddacd9d7a0be336f69b4
Tom L <devzemux1613@gmail.com>
2022-08-18 21:33:13 +0200

    fix: remove system exit

--- src/main/java/de/tom/equationSolver/EquationSolver.java
+++ src/main/java/de/tom/equationSolver/EquationSolver.java
@@ -4,10 +4,7 @@ public class EquationSolver {
 
     public static void main(String[] args) {
 
-        if(args.length == 0){
-            System.out.println("Es wurde keine Gleichung angegeben!");
-            System.exit(0);
-        }
+
 
     }
 
