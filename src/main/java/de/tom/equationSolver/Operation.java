9acd17110eb429be33a95941f755b17c4233071a
Tom L <devzemux1613@gmail.com>
2022-08-19 10:12:40 +0200

    feat: add symbol

--- src/main/java/de/tom/equationSolver/Operation.java
+++ src/main/java/de/tom/equationSolver/Operation.java
@@ -1,8 +1,18 @@
 package de.tom.equationSolver;
 
 public enum Operation {
-    ADDITION,
-    SUBTRACTION,
-    MULTIPLIKATION,
-    DIVISION;
+    ADDITION("+"),
+    SUBTRACTION("-"),
+    MULTIPLIKATION("*"),
+    DIVISION("/");
+
+    private String symbol;
+
+    Operation(String symbol){
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
