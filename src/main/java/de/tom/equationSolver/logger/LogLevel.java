774312fe35f818f3d6e321b83a7d98a558a00b16
Tom L <devzemux1613@gmail.com>
2022-08-19 19:24:49 +0200

    feat: add LogLevels

---
+++ src/main/java/de/tom/equationSolver/logger/LogLevel.java
@@ -0,0 +1,15 @@
+package de.tom.equationSolver.logger;
+
+public enum LogLevel {
+
+    WARNING("[WARNING] "),
+    INFO("[INFO] "),
+    ERROR("[ERROR] ");
+
+    private String prefix;
+
+    LogLevel(final String prefix) {
+        this.prefix = prefix;
+    }
+
+}
